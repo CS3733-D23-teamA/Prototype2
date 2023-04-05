030c89027b62f8acd8d40c889fc37a656ee267c5
ashleigh <amperez@wpi.edu>
2023-04-05 00:04:05 -0400

    Exit menu functionality and resizing issue started

--- src/main/java/edu/wpi/teamA/controllers/HeaderController.java
+++ src/main/java/edu/wpi/teamA/controllers/HeaderController.java
@@ -2,7 +2,9 @@ package edu.wpi.teamA.controllers;
 
 import edu.wpi.teamA.navigation.Navigation;
 import edu.wpi.teamA.navigation.Screen;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.scene.control.DialogPane;
 import javafx.scene.control.MenuButton;
 import javafx.scene.control.MenuItem;
 import javafx.scene.image.ImageView;
@@ -19,6 +21,7 @@ public class HeaderController {
   @FXML private MenuItem exitMenu;
   @FXML private ImageView bwhLogo;
   @FXML private Circle profile;
+  @FXML private DialogPane exitDialog;
 
   @FXML
   public void initialize() {
@@ -27,8 +30,15 @@ public class HeaderController {
     mapMenu.setOnAction(event -> Navigation.navigate(Screen.MAP));
     signageMenu.setOnAction(event -> Navigation.navigate(Screen.SIGNAGE));
     pathfindingMenu.setOnAction(event -> Navigation.navigate(Screen.PATHFINDING));
-    // exit menu
+    exitMenu.setOnAction(event -> exit());
     bwhLogo.setOnMouseClicked(event -> Navigation.navigate(Screen.HOME));
     // profile.setOnMouseClicked();
+  }
+
+  public void exit() {
+    // if (JOptionPane.showConfirmDialog( Frame,"confirm if you Want to Exit","Name of the
+    // Application or Title",
+    // JOptionPane.YES_NO_OPTION)==JOptionPane.YES_OPTION)
+    Platform.exit();
   }
 }
