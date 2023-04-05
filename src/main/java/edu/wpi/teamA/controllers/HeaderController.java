a7834ac63b935b282e89c5ce0aef7a365649dc9e
Seth Frank <sbfrank@wpi.edu>
2023-04-05 08:53:43 -0400

    Added functionality to exit menu

--- src/main/java/edu/wpi/teamA/controllers/HeaderController.java
+++ src/main/java/edu/wpi/teamA/controllers/HeaderController.java
@@ -1,5 +1,6 @@
 package edu.wpi.teamA.controllers;
 
+import edu.wpi.teamA.App;
 import edu.wpi.teamA.navigation.Navigation;
 import edu.wpi.teamA.navigation.Screen;
 import javafx.fxml.FXML;
@@ -27,7 +28,7 @@ public class HeaderController {
     mapMenu.setOnAction(event -> Navigation.navigate(Screen.MAP));
     signageMenu.setOnAction(event -> Navigation.navigate(Screen.SIGNAGE));
     pathfindingMenu.setOnAction(event -> Navigation.navigate(Screen.PATHFINDING));
-    // exit menu
+    exitMenu.setOnAction(event -> App.getPrimaryStage().hide());
     bwhLogo.setOnMouseClicked(event -> Navigation.navigate(Screen.HOME));
     // profile.setOnMouseClicked();
   }
