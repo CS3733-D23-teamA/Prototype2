093cd255d319f345b31a7297a882e1f1a1324cb3
Vincent Boothroyd <vincentboothroyd@gmail.com>
2023-04-05 22:33:21 -0400

    Fixed my git mistake

--- src/test/java/edu/wpi/teamA/AStarTest.java
+++ src/test/java/edu/wpi/teamA/AStarTest.java
@@ -22,11 +22,11 @@ public class AStarTest {
   protected void setUp() throws Exception {
     System.out.println("Setting it up!");
     GraphNode nodeA = new GraphNode(11, 0, 0, "", "");
-    GraphNode nodeB = new GraphNode(12, 0, 0, "", "");
-    GraphNode nodeC = new GraphNode(13, 0, 0, "", "");
-    GraphNode nodeD = new GraphNode(14, 0, 0, "", "");
-    GraphNode nodeE = new GraphNode(15, 0, 0, "", "");
-    GraphNode nodeF = new GraphNode(16, 0, 0, "", "");
+    GraphNode nodeB = new GraphNode(12, 0, 5, "", "");
+    GraphNode nodeC = new GraphNode(13, 5, 5, "", "");
+    GraphNode nodeD = new GraphNode(14, 50, 50, "", "");
+    GraphNode nodeE = new GraphNode(15, 10, 10, "", "");
+    GraphNode nodeF = new GraphNode(16, 3, 2, "", "");
 
     graph.addNodeToGraph(nodeA);
     graph.addNodeToGraph(nodeB);
@@ -36,21 +36,24 @@ public class AStarTest {
     graph.addNodeToGraph(nodeF);
     Edge edgeAB = new Edge(nodeA.getNodeID(), nodeB.getNodeID());
     Edge edgeBC = new Edge(nodeB.getNodeID(), nodeC.getNodeID());
-    Edge edgeCD = new Edge(nodeC.getNodeID(), nodeD.getNodeID());
-    Edge edgeAE = new Edge(nodeA.getNodeID(), nodeE.getNodeID());
-    Edge edgeDF = new Edge(nodeD.getNodeID(), nodeF.getNodeID());
+    Edge edgeCE = new Edge(nodeC.getNodeID(), nodeE.getNodeID());
+    Edge edgeAD = new Edge(nodeA.getNodeID(), nodeD.getNodeID());
+    Edge edgeDE = new Edge(nodeD.getNodeID(), nodeE.getNodeID());
+    Edge edgeBF = new Edge(nodeB.getNodeID(), nodeF.getNodeID());
     // Edge edgeAC = new Edge(nodeA.getNodeID(), nodeC.getNodeID());
 
     nodeA.addEdge(edgeAB);
     nodeB.addEdge(edgeAB);
     nodeB.addEdge(edgeBC);
     nodeC.addEdge(edgeBC);
-    nodeA.addEdge(edgeAE);
-    nodeE.addEdge(edgeAE);
-    nodeC.addEdge(edgeCD);
-    nodeD.addEdge(edgeCD);
-    nodeF.addEdge(edgeDF);
-    nodeD.addEdge(edgeDF);
+    nodeC.addEdge(edgeCE);
+    nodeE.addEdge(edgeCE);
+    nodeA.addEdge(edgeAD);
+    nodeD.addEdge(edgeAD);
+    nodeD.addEdge(edgeDE);
+    nodeE.addEdge(edgeDE);
+    nodeB.addEdge(edgeBF);
+    nodeF.addEdge(edgeBF);
     // nodeA.addEdge(edgeAC);
     // nodeC.addEdge(edgeAC);
   }
@@ -62,7 +65,7 @@ public class AStarTest {
     expected_path.add(11);
     expected_path.add(12);
     expected_path.add(13);
-    ArrayList<Integer> actual_path = aStar1.setPath();
+    ArrayList<Integer> actual_path = aStar1.getPath();
     assertEquals(expected_path, actual_path);
 
     AStar aStar2 = new AStar(graph, 13, 11);
@@ -70,24 +73,24 @@ public class AStarTest {
     expected_path.add(13);
     expected_path.add(12);
     expected_path.add(11);
-    actual_path = aStar2.setPath();
+    actual_path = aStar2.getPath();
     assertEquals(expected_path, actual_path);
 
     AStar aStar3 = new AStar(graph, 11, 15);
     expected_path = new ArrayList<Integer>();
     expected_path.add(11);
+    expected_path.add(12);
+    expected_path.add(13);
     expected_path.add(15);
-    actual_path = aStar3.setPath();
+    actual_path = aStar3.getPath();
     assertEquals(expected_path, actual_path);
 
     AStar aStar4 = new AStar(graph, 11, 16);
     expected_path = new ArrayList<Integer>();
     expected_path.add(11);
     expected_path.add(12);
-    expected_path.add(13);
-    expected_path.add(14);
     expected_path.add(16);
-    actual_path = aStar4.setPath();
+    actual_path = aStar4.getPath();
     assertEquals(expected_path, actual_path);
   }
 }
