e0ccbeb8fc6ee1febea28b3bec021f3a5aa28507
alex <ajsamra@wpi.edu>
2023-04-11 12:24:45 -0400

    Finished "My Service Requests page"

--- src/main/java/edu/wpi/teamA/database/Interfaces/ICRRRDAO.java
+++ src/main/java/edu/wpi/teamA/database/Interfaces/ICRRRDAO.java
@@ -1,3 +1,16 @@
 package edu.wpi.teamA.database.Interfaces;
 
-public interface ICRRRDAO {}
+import edu.wpi.teamA.database.ORMclasses.ConferenceRoomResRequest;
+import java.util.List;
+
+public interface ICRRRDAO {
+  public List<ConferenceRoomResRequest> getAllCRRR();
+
+  public ConferenceRoomResRequest getCRRR(String name);
+
+  public void updateCRRR(ConferenceRoomResRequest room);
+
+  public void addCRRR(ConferenceRoomResRequest room);
+
+  public void deleteCRRR(ConferenceRoomResRequest room);
+}
