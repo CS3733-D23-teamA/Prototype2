f67626d7e3941840e8e744fc89e8bfc13897766b
alex <ajsamra@wpi.edu>
2023-04-10 21:52:21 -0400

    fixed time issue

--- src/main/java/edu/wpi/teamA/controllers/Navigation/ServiceRequestController.java
+++ src/main/java/edu/wpi/teamA/controllers/Navigation/ServiceRequestController.java
@@ -1,20 +1,49 @@
 package edu.wpi.teamA.controllers.Navigation;
 
+import edu.wpi.teamA.database.DAOImps.FlowerDAOImpl;
+import edu.wpi.teamA.database.ORMclasses.FlowerEntity;
 import edu.wpi.teamA.navigation.Navigation;
 import edu.wpi.teamA.navigation.Screen;
+import java.util.List;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
+import javafx.scene.control.SelectionMode;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 public class ServiceRequestController extends PageController {
-  @FXML
-  public void initialize() {}
+  private FlowerDAOImpl fdao = new FlowerDAOImpl();
+  @FXML private TableView<FlowerEntity> flowerTable;
+  @FXML private TableColumn<FlowerEntity, String> roomCol;
+  @FXML private TableColumn<FlowerEntity, String> dateCol;
+  @FXML private TableColumn<FlowerEntity, String> timeCol;
+  @FXML private TableColumn<FlowerEntity, String> flowerCol;
+  @FXML private TableColumn<FlowerEntity, String> commentCol;
+  @FXML private TableColumn<FlowerEntity, String> statusCol;
 
   @FXML
-  public void flowerPage() {
+  public void initialize() {
+    displayFlowerRequests(fdao.getAllFlowers());
+  }
+
+  public void displayFlowerRequests(List<FlowerEntity> flowerEntityArrayList) {
+    roomCol.setCellValueFactory(new PropertyValueFactory<>("room"));
+    dateCol.setCellValueFactory(new PropertyValueFactory<>("date"));
+    timeCol.setCellValueFactory(new PropertyValueFactory<>("time"));
+    flowerCol.setCellValueFactory(new PropertyValueFactory<>("flowerType"));
+    commentCol.setCellValueFactory(new PropertyValueFactory<>("comment"));
+    statusCol.setCellValueFactory(new PropertyValueFactory<>("status"));
+
+    flowerTable.setItems(FXCollections.observableArrayList(flowerEntityArrayList));
+    flowerTable.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+  }
+
+  public void flowerMenu() {
     Navigation.navigate(Screen.FLOWER_REQUEST);
   }
 
-  @FXML
-  public void conferencePage() {
-    Navigation.navigate(Screen.CONFERENCE_REQUEST);
+  public void roomMenu() {
+    Navigation.navigate(Screen.FLOWER_REQUEST);
   }
 }
--- src/main/java/edu/wpi/teamA/database/DAOImps/FlowerDAOImpl.java
+++ src/main/java/edu/wpi/teamA/database/DAOImps/FlowerDAOImpl.java
@@ -5,6 +5,7 @@ import edu.wpi.teamA.database.Interfaces.IFlowerDAO;
 import edu.wpi.teamA.database.ORMclasses.FlowerEntity;
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.List;
 
 public class FlowerDAOImpl implements IFlowerDAO {
   ArrayList<FlowerEntity> flowerArray;
@@ -32,7 +33,7 @@ public class FlowerDAOImpl implements IFlowerDAO {
 
       String sqlCreateEdge =
           "Create Table if not exists \"Prototype2_schema\".\"Flower\""
-              + "(namee   Varchar(600),"
+              + "(namee    Varchar(600),"
               + "room    int,"
               + "datee    date,"
               + "timee     int,"
@@ -82,8 +83,107 @@ public class FlowerDAOImpl implements IFlowerDAO {
   }
 
   @Override
-  public void updateFlower(FlowerEntity flower) {}
+  public List<FlowerEntity> getAllFlowers() {
+    ArrayList<FlowerEntity> tempList = new ArrayList<>();
+    try {
+      Statement stmt = flowerProvider.createConnection().createStatement();
+      ResultSet rs = stmt.executeQuery("SELECT * FROM \"Prototype2_schema\".\"Flower\"");
+
+      while (rs.next()) {
+        String namee = rs.getString("namee");
+        int room = rs.getInt("room");
+        Date date = rs.getDate("datee");
+        int time = rs.getInt("timee");
+        String flowerType = rs.getString("flowertype");
+        String comment = rs.getString("comment");
+        String status = rs.getString("status");
+
+        FlowerEntity temp = new FlowerEntity();
+        temp.setName(namee);
+        temp.setRoom(room);
+        temp.setDate(date);
+        temp.setTime(time);
+        temp.setComment(comment);
+        temp.setStatus(status);
+
+        tempList.add(temp);
+      }
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
+    }
+    return tempList;
+  }
 
   @Override
-  public void editFlower(FlowerEntity flower) {}
+  public FlowerEntity getFlower(String name) {
+    FlowerEntity temp = new FlowerEntity();
+    try {
+      PreparedStatement ps =
+          flowerProvider
+              .createConnection()
+              .prepareStatement("SELECT FROM \"Prototype2_schema\".\"Flower\" WHERE namee = ?");
+      ps.setString(1, name);
+      ResultSet rs = ps.executeQuery();
+      if (rs.next()) {
+        String idk = rs.getString("namee");
+        int room = rs.getInt("room");
+        Date date = rs.getDate("datee");
+        int time = rs.getInt("timee");
+        String flowerType = rs.getString("flowertype");
+        String comment = rs.getString("comment");
+        String status = rs.getString("status");
+
+        temp.setName(idk);
+        temp.setRoom(room);
+        temp.setDate(date);
+        temp.setTime(time);
+        temp.setComment(comment);
+        temp.setStatus(status);
+      }
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
+    }
+    return temp;
+  }
+
+  @Override
+  public void updateFlower(FlowerEntity flower) {
+    try {
+      int room = flower.getRoom();
+      Date date = flower.getDate();
+      int time = flower.getTime();
+      String type = flower.getFlowerType();
+      String comment = flower.getComment();
+      String status = flower.getStatus();
+
+      PreparedStatement ps =
+          flowerProvider
+              .createConnection()
+              .prepareStatement(
+                  "UPDATE Prototype2_schema.\"Flower\" SET room = ?, datee = ?, timee = ?, flowerType = ?, comment = ?, status = ? WHERE namee = ?");
+      ps.setInt(1, room);
+      ps.setDate(2, date);
+      ps.setInt(3, time);
+      ps.setString(4, type);
+      ps.setString(5, comment);
+      ps.setString(5, status);
+      ps.executeUpdate();
+
+      flowerArray.forEach(
+          FlowerEntity -> {
+            if (FlowerEntity.getName().equals(flower.getName())) {
+              FlowerEntity.setRoom(room);
+              FlowerEntity.setDate(date);
+              FlowerEntity.setTime(time);
+              FlowerEntity.setComment(comment);
+              FlowerEntity.setStatus(status);
+            }
+          });
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  @Override
+  public void editFlower(FlowerEntity flower, int field) {}
 }
